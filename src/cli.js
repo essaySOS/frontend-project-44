1ca9067f91d417d86fd444a81826b03322fef980
essaySOS <corama.2000@mail.ru>
2024-01-19 13:49:58 +0300

    4

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import * as readlineSync from 'readline-sync';
+import * as readlineSync from "readline-sync";
 
 const hi = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`${'Hello,'} ${userName}${'!'}`);
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`${"Hello,"} ${userName}${"!"}`);
 };
 
 export default hi;
